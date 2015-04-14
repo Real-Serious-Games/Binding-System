f89dc91b9bd8f8eac8aa9f2d6a8a84d4e9fd2d68
Philip Warren <philip.warren@realseriousgames.com>
2015-04-14 15:33:04 +1000

    Added the main class of the system to the repository.

--- Src/BindingManager.cs
+++ Src/BindingManager.cs
@@ -1,11 +1,119 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using RSG.Utils;
 
 namespace RSG
 {
-    class BindingManager
+    /// <summary>
+    /// Application of the Facade pattern. IBindingManager provides an insulated entry point to the binding system.
+    /// The binding system is used to monitor an object tree for changes.
+    /// </summary>
+    public interface IBindingManager
     {
+        /// <summary>
+        /// Disconnect the binding
+        /// </summary>
+        void Disconnect();
+
+        /// <summary>
+        /// Find a binding nested in the tree of property bidnings.
+        /// </summary>
+        IBinding FindNestedBinding(string bindingName);
+
+        /// <summary>
+        /// Stream of property changing events.
+        /// </summary>
+        IObservable<BoundPropertyChangingEventArgs> PropertyChangingEventStream { get; }
+
+        /// <summary>
+        /// Stream of property changed events.
+        /// </summary>
+        IObservable<BoundPropertyChangedEventArgs> PropertyChangedEventStream { get; }
+
+        /// <summary>
+        /// Event raised when a value in the binding tree has changed.
+        /// </summary>
+        IObservable<BoundCollectionChangedEventArgs> CollectionChangedEventStream { get; }
+    }
+
+    /// <summary>
+    /// Application of the Facade pattern. IBindingManager provides an insulated entry point to the binding system.
+    /// The binding system is used to monitor an object tree for changes.
+    /// </summary>
+    public class BindingManager : IBindingManager
+    {
+        /// <summary>
+        /// Binds to the root object in the object graph.
+        /// </summary>
+        private ObjectBinding binding;
+
+        /// <summary>
+        /// Stream of property changing events.
+        /// </summary>
+        public IObservable<BoundPropertyChangingEventArgs> PropertyChangingEventStream
+        {
+            get
+            {
+                return binding.PropertyChangingEventStream;
+            }
+        }
+
+        /// <summary>
+        /// Stream of property changed events.
+        /// </summary>
+        public IObservable<BoundPropertyChangedEventArgs> PropertyChangedEventStream
+        {
+            get
+            {
+                return binding.PropertyChangedEventStream;
+            }
+        }
+
+        /// <summary>
+        /// Event raised when a value in the binding tree has changed.
+        /// </summary>
+        public IObservable<BoundCollectionChangedEventArgs> CollectionChangedEventStream
+        {
+            get
+            {
+                return binding.CollectionChangedEventStream;
+            }
+        }
+
+
+        public BindingManager(object bindingObject)
+        {
+            Argument.NotNull(() => bindingObject);
+            
+            this.binding = new ObjectBinding(bindingObject, new BindingsFactory());
+        }
+
+        /// <summary>
+        /// Stop monitoring object graph.
+        /// </summary>
+        public void Disconnect()
+        {
+            if (binding == null)
+            {
+                return;
+            }
+
+            binding.Disconnect();
+
+            binding = null;
+        }
+
+        /// <summary>
+        /// Find a binding nested in the tree of property binfings.
+        /// </summary>
+        public IBinding FindNestedBinding(string bindingName)
+        {
+            Argument.StringNotNullOrEmpty(() => bindingName);
+
+            return binding.FindNestedBinding(bindingName);
+        }
     }
 }
---
+++ Src/ObjectBinding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSG
+{
+    public class ObjectBinding
+    {
+        private object bindingObject;
+        private BindingsFactory bindingsFactory;
+
+        public ObjectBinding(object bindingObject, BindingsFactory bindingsFactory)
+        {
+            // TODO: Complete member initialization
+            this.bindingObject = bindingObject;
+            this.bindingsFactory = bindingsFactory;
+        }
+
+        public IObservable<BoundPropertyChangingEventArgs> PropertyChangingEventStream { get; set; }
+
+        public IObservable<BoundPropertyChangedEventArgs> PropertyChangedEventStream { get; set; }
+
+        public IObservable<BoundCollectionChangedEventArgs> CollectionChangedEventStream { get; set; }
+
+        internal void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IBinding FindNestedBinding(string bindingName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
