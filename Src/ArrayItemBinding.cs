216df35c711975012b15bfe1bc2d8e086409c766
Philip Warren <philip.warren@realseriousgames.com>
2015-05-26 09:05:21 +1000

    Added ArrayItemBinding class,

---
+++ Src/ArrayItemBinding.cs
@@ -0,0 +1,188 @@
+﻿using RSG.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace RSG
+{
+    /// <summary>
+    /// A binding to an item in an array.
+    /// </summary>
+    public interface IArrayItemBinding : IBinding
+    {
+        /// <summary>
+        /// The index of the bound list item.
+        /// </summary>
+        int ItemIndex { get; set; }
+    }
+
+    /// <summary>
+    /// A binding to an item in a list.
+    /// </summary>
+    public class ArrayItemBinding : IArrayItemBinding
+    {
+        /// <summary>
+        /// The index of the bound list item.
+        /// </summary>
+        public int ItemIndex { get; set; }
+
+        /// <summary>
+        /// The list that bindings are managed for.
+        /// </summary>
+        private Array parentArray;
+
+        /// <summary>
+        /// The value binding that is named.
+        /// </summary>
+        public IValueBinding ValueBinding { get; private set; }
+
+        /// <summary>
+        /// Stream of property changing events.
+        /// </summary>
+        public IObservable<BoundPropertyChangingEventArgs> PropertyChangingEventStream
+        {
+            get
+            {
+                return ValueBinding.PropertyChangingEventStream
+                    .Select(ev => new BoundPropertyChangingEventArgs(GenerateAggregatePropertyName(ev.PropertyName), ev.Binding));
+            }
+        }
+
+        /// <summary>
+        /// Stream of property changed events.
+        /// </summary>
+        public IObservable<BoundPropertyChangedEventArgs> PropertyChangedEventStream
+        {
+            get
+            {
+                return ValueBinding.PropertyChangedEventStream
+                    .Select(ev => new BoundPropertyChangedEventArgs(GenerateAggregatePropertyName(ev.PropertyName), ev.Binding));
+            }
+        }
+
+        /// <summary>
+        /// Stream of collection changed events.
+        /// </summary>
+        public IObservable<BoundCollectionChangedEventArgs> CollectionChangedEventStream
+        {
+            get
+            {
+                return ValueBinding.CollectionChangedEventStream
+                    .Select(ev => new BoundCollectionChangedEventArgs(GenerateAggregatePropertyName(ev.PropertyName), ev.SourceEventArgs, ev.Binding, ev.PreviousCollectionContent));
+            }
+        }
+
+        public ArrayItemBinding(int itemIndex, Array parentArray, IValueBinding valueBinding)
+        {
+            Argument.Invariant(() => itemIndex, () => itemIndex >= 0);
+            Argument.NotNull(() => parentArray);
+            Argument.NotNull(() => valueBinding);
+
+
+            if (itemIndex < 0)
+            {
+                throw new ArgumentException("Item index can't be less than 0.", "itemIndex");
+            }
+
+            this.ItemIndex = itemIndex;
+            this.ValueBinding = valueBinding;
+
+            // Don't need to connect, the value binding will already be connected.
+            this.parentArray = parentArray;
+        }
+
+        /// <summary>
+        /// Retreive the value.
+        /// </summary>
+        public object GetValue()
+        {
+            return this.parentArray.GetValue(this.ItemIndex);
+        }
+
+        /// <summary>
+        /// Set the value.
+        /// </summary>
+        public void SetValue(object newValue)
+        {
+            this.parentArray.SetValue(newValue, this.ItemIndex);
+        }
+
+        /// <summary>
+        /// The type of the binding.
+        /// </summary>
+        public Type BindingType
+        {
+            get
+            {
+                return this.parentArray.GetType().GetElementType();
+            }
+        }
+
+        /// <summary>
+        /// Reattach the property to the property in the specified object.
+        /// </summary>
+        public void Connect(object parentObj)
+        {
+            if (parentObj == null)
+            {
+                throw new ArgumentNullException("parentObj");
+            }
+
+            Disconnect();
+
+            this.parentArray = (Array)parentObj;
+
+            // 
+            // Rebind if non-null.
+            //
+            var obj = this.GetValue();
+            if (obj != null)
+            {
+                this.ValueBinding.Connect(obj);
+            }
+        }
+
+        /// <summary>
+        /// Disconnected events from the bound property.
+        /// </summary>
+        public void Disconnect()
+        {
+            if (this.parentArray == null)
+            {
+                return;
+            }
+
+            this.ValueBinding.Disconnect();
+
+            this.parentArray = null;
+        }
+
+        /// <summary>
+        /// Find a binding nested in the tree of property bidnings.
+        /// </summary>
+        public IBinding FindNestedBinding(string bindingName)
+        {
+            if (string.IsNullOrEmpty(bindingName))
+            {
+                throw new ArgumentException("Bad bindingName input.", "bindingName");
+            }
+
+            return this.ValueBinding.FindNestedBinding(bindingName);
+        }
+
+        /// <summary>
+        /// Helper to concatenate property names.
+        /// </summary>
+        private string GenerateAggregatePropertyName(string propertyName)
+        {
+            var itemIndexStr = this.ItemIndex.ToString();
+
+            return
+                !string.IsNullOrEmpty(propertyName) ?
+                    itemIndexStr + "." + propertyName :
+                    itemIndexStr;
+        }
+    }
+}
---
+++ Tests/ArrayItemBindingTests.cs
@@ -0,0 +1,274 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using Xunit;
+
+namespace RSG.Tests
+{
+    public class ArrayItemBindingTests
+    {
+        public class TestClass
+        {
+            public int X 
+            { 
+                get
+                {
+                    return x;
+                }
+                set
+                {
+                    x = value;
+                }
+            }
+            private int x = 0;
+        }
+
+        private Mock<IValueBinding> mockValueBinding;
+
+        private ArrayItemBinding testObject;
+
+        private TestClass[] testArray;
+
+        private void Init()
+        {
+            InitMocks();
+
+            InitTestObject(1, 0);
+        }
+
+        private void InitMocks()
+        {
+            mockValueBinding = new Mock<IValueBinding>();
+        }
+
+        private void InitTestObject(int lenthOfArray, int index)
+        {
+            testArray = Enumerable.Range(0, lenthOfArray)
+                .Select(i => new TestClass())
+                .ToArray();
+
+            testObject = new ArrayItemBinding(index, testArray, mockValueBinding.Object);
+        }
+
+
+        [Fact]
+        public void disconnect()
+        {
+            Init();
+            
+            mockValueBinding.Verify(m => m.Connect(testArray[0]), Times.Never());
+
+            testObject.Disconnect();
+
+            mockValueBinding.Verify(m => m.Disconnect(), Times.Once());
+        }
+
+        [Fact]
+        public void reconnect()
+        {
+            Init();
+
+            var otherList = new TestClass[]
+            {
+                new TestClass()
+                {
+                    X = 10
+                }
+            };
+
+            testObject.Connect(otherList);
+
+            mockValueBinding.Verify(m => m.Disconnect(), Times.Once());
+
+            mockValueBinding.Verify(m => m.Connect(otherList[0]), Times.Once());
+        }
+
+        [Fact]
+        public void explicit_disconnect_and_reconnect()
+        {
+            Init();
+
+            testObject.Disconnect();
+
+            var otherList = new TestClass[]
+            {
+                new TestClass()
+                {
+                    X = 10
+                }
+            };
+
+            testObject.Connect(otherList);
+
+            mockValueBinding.Verify(m => m.Disconnect(), Times.Once());
+
+            mockValueBinding.Verify(m => m.Connect(otherList[0]), Times.Once());
+        }
+
+        [Fact]
+        public void get_value_index_0()
+        {
+            Init();
+
+            Assert.Equal(testArray[0], testObject.GetValue());
+        }
+
+        [Fact]
+        public void get_value_index_1()
+        {
+            InitMocks();
+
+            InitTestObject(2, 1);
+
+            Assert.Equal(testArray[1], testObject.GetValue());
+        }
+
+        [Fact]
+        public void set_value_index_0()
+        {
+            Init();
+
+            Assert.Equal(0, testArray[0].X);
+
+            testObject.SetValue(new TestClass() { X = 32 });
+
+            Assert.Equal(32, testArray[0].X);
+        }
+
+        [Fact]
+        public void set_value_index_1()
+        {
+            InitMocks();
+
+            InitTestObject(2, 1);
+
+            Assert.Equal(0, testArray[1].X);
+
+            testObject.SetValue(new TestClass() { X = 32 });
+
+            Assert.Equal(32, testArray[1].X);
+        }
+
+        [Fact]
+        public void binding_type()
+        {
+            Init();
+
+            Assert.Equal(typeof(TestClass), testObject.BindingType);
+        }
+
+        [Fact]
+        public void FindNestedBinding_with_null_string()
+        {
+            Init();
+
+            Assert.Throws<ArgumentException>(() =>
+                testObject.FindNestedBinding(null)
+            );
+        }
+
+        [Fact]
+        public void FindNestedBinding_with_empty_string()
+        {
+            Init();
+
+            Assert.Throws<ArgumentException>(() =>
+                testObject.FindNestedBinding(string.Empty)
+            );
+        }
+
+        [Fact]
+        public void find_nested_binding()
+        {
+            InitMocks();
+            
+            var mockNestedBinding = new Mock<IBinding>();
+
+            mockValueBinding
+                .Setup(m => m.FindNestedBinding("Other"))
+                .Returns(mockNestedBinding.Object);
+
+            InitTestObject(1, 0);
+
+            Assert.Equal(mockNestedBinding.Object, testObject.FindNestedBinding("Other"));
+
+            mockValueBinding.Verify(m => m.FindNestedBinding("Other"));
+        }
+
+        [Fact]
+        public void BoundPropertyChanging_event_from_child_passed_on()
+        {
+            InitMocks();
+
+            var childEventStream = new Subject<BoundPropertyChangingEventArgs>();
+            var mockChildBinding = new Mock<IBinding>();
+
+            mockValueBinding
+                .Setup(m => m.PropertyChangingEventStream)
+                .Returns(childEventStream);
+
+            InitTestObject(1, 0);
+
+            var events = new List<BoundPropertyChangingEventArgs>();
+            testObject.PropertyChangingEventStream.Subscribe(ev => events.Add(ev));
+
+            childEventStream.OnNext(new BoundPropertyChangingEventArgs("Nested", mockChildBinding.Object));
+
+            Assert.Equal(1, events.Count);
+            Assert.Equal("0.Nested", events[0].PropertyName);
+            Assert.Equal(mockChildBinding.Object, events[0].Binding);
+        }
+
+        [Fact]
+        public void BoundPropertyChanged_event_from_child_passed_on()
+        {
+            InitMocks();
+
+            var childEventStream = new Subject<BoundPropertyChangedEventArgs>();
+            var mockChildBinding = new Mock<IObjectPropertyBinding>();
+
+            mockValueBinding
+                .Setup(m => m.PropertyChangedEventStream)
+                .Returns(childEventStream);
+
+            InitTestObject(1, 0);
+
+            var events = new List<BoundPropertyChangedEventArgs>();
+            testObject.PropertyChangedEventStream.Subscribe(ev => events.Add((BoundPropertyChangedEventArgs)ev));
+
+            childEventStream.OnNext(new BoundPropertyChangedEventArgs("Nested", mockChildBinding.Object));
+
+            Assert.Equal(1, events.Count);
+            Assert.Equal("0.Nested", events[0].PropertyName);
+            Assert.Equal(mockChildBinding.Object, events[0].Binding);
+        }
+
+        [Fact]
+        public void BoundCollectionChanged_event_from_child_passed_on()
+        {
+            InitMocks();
+
+            var childEventStream = new Subject<BoundCollectionChangedEventArgs>();
+
+            mockValueBinding
+                .Setup(m => m.CollectionChangedEventStream)
+                .Returns(childEventStream);
+
+            InitTestObject(1, 0);
+
+            var events = new List<BoundCollectionChangedEventArgs>();
+            testObject.CollectionChangedEventStream.Subscribe(ev => events.Add(ev));
+
+            var fakeCollectionChangedEventArgs = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+            childEventStream.OnNext(new BoundCollectionChangedEventArgs("Nested", fakeCollectionChangedEventArgs, null, null));
+
+            Assert.Equal(1, events.Count);
+            Assert.Equal("0.Nested", events[0].PropertyName);
+            Assert.Equal(fakeCollectionChangedEventArgs, events[0].SourceEventArgs);
+        }
+    }
+}
