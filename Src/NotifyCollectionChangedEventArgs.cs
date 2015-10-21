2e5b1f606b618982f14037db790b8ed2d0396b7c
Philip Warren <philip.warren@realseriousgames.com>
2015-10-21 11:39:19 +1000

    Implemented the NotifyCollectionChangedEventArgs class.

--- Src/NotifyCollectionChangedEventArgs.cs
+++ Src/NotifyCollectionChangedEventArgs.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections;
 
 namespace RSG
 {
     public enum NotifyCollectionChangedAction
     {
+        Add,
+        Remove,
         Reset,
     }
 
@@ -15,11 +15,50 @@ namespace RSG
     /// </summary>
     public class NotifyCollectionChangedEventArgs : EventArgs
     {
-        private NotifyCollectionChangedAction action;
+        // Summary:
+        //     Gets the action that caused the event.
+        //
+        // Returns:
+        //     A System.Collections.Specialized.NotifyCollectionChangedAction value that
+        //     describes the action that caused the event.
+        public NotifyCollectionChangedAction Action { get; private set; }
+
+        // Summary:
+        //     Gets the list of new items involved in the change.
+        //
+        // Returns:
+        //     The list of new items involved in the change.
+        public IList NewItems { get; private set; }
+
+        // Summary:
+        //     Gets the index at which the change occurred.
+        //
+        // Returns:
+        //     The zero-based index at which the change occurred.
+        public int NewStartingIndex { get; private set; }
+
+        // Summary:
+        //     Gets the list of items affected by a System.Collections.Specialized.NotifyCollectionChangedAction.Replace,
+        //     Remove, or Move action.
+        //
+        // Returns:
+        //     The list of items affected by a System.Collections.Specialized.NotifyCollectionChangedAction.Replace,
+        //     Remove, or Move action.
+        public IList OldItems { get; private set; }
+
+        // Summary:
+        //     Gets the index at which a System.Collections.Specialized.NotifyCollectionChangedAction.Move,
+        //     Remove, or Replace action occurred.
+        //
+        // Returns:
+        //     The zero-based index at which a System.Collections.Specialized.NotifyCollectionChangedAction.Move,
+        //     Remove, or Replace action occurred.
+        public int OldStartingIndex { get; private set; }
 
         public NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction action)
         {
-            this.action = action;
+            this.Action = action;
         }
+
     }
 }
